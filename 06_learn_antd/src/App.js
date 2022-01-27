d286a676f287ba71202f96afb1a851048ebcc6ab
travis <travis.x.wong@gmail.com>
2022-01-27 23:02:39 +0800

    antd 初体验

--- 06_learn_antd/src/App.js
+++ 06_learn_antd/src/App.js
@@ -1,29 +1,69 @@
 import React from "react";
 import classNames from 'classnames'
+import { Button, Space } from 'antd';
+import { PoweroffOutlined } from '@ant-design/icons'
 
 class App extends React.Component {
   constructor(){
     super()
     this.state = {
       foo: true,
-      isActive: true
+      isActive: true,
+      loadings: true
     }
   }
 
   render(){
-    const {foo, isActive} = this.state
+    const {loadings} = this.state
     return (
-    <div>
-      {/* 常规 React 添加class方法 */}
-      <h2 className={"hello " + (isActive ? "world" : "")}>标题1</h2>
-      <h2 className="foo bar active title">标题1</h2>
+    <>
+      <Space style={{ width: '100%' }}>
+        <Button type="primary" loading>
+          Loading
+        </Button>
+        <Button type="primary" size="small" loading>
+          Loading
+        </Button>
+        <Button type="primary" icon={<PoweroffOutlined />} loading />
+      </Space>
+
+      <Space style={{ width: '100%' }}>
+        <Button type="primary" loading={loadings[0]} onClick={() => this.enterLoading(0)}>
+          Click me!
+        </Button>
+        <Button
+          type="primary"
+          icon={<PoweroffOutlined />}
+          loading={loadings[1]}
+          onClick={() => this.enterLoading(1)}
+        >
+          Click me!
+        </Button>
+        <Button
+          type="primary"
+          icon={<PoweroffOutlined />}
+          loading={loadings[2]}
+          onClick={() => this.enterLoading(2)}
+        />
+      </Space>
+    </>
+    )
+  }
+
+  // render(){
+  //   const {foo, isActive} = this.state
+  //   return (
+  //   <div>
+  //     {/* 常规 React 添加class方法 */}
+  //     <h2 className={"hello " + (isActive ? "world" : "")}>标题1</h2>
+  //     <h2 className="foo bar active title">标题1</h2>
       
-      {/* 使用classnames */}
-      <h2 className={classNames({hello: foo, active: isActive})}>标题1</h2>
-      <h2 className={classNames('a', 'b', 'c')}>标题1</h2>
-      <h2 className={classNames({hello: foo}, 'world')}>标题1</h2>
-    </div>
-  )}
+  //     {/* 使用classnames */}
+  //     <h2 className={classNames({hello: foo, active: isActive})}>标题1</h2>
+  //     <h2 className={classNames('a', 'b', 'c')}>标题1</h2>
+  //     <h2 className={classNames({hello: foo}, 'world')}>标题1</h2>
+  //   </div>
+  // )}
 }
 
 export default App;
